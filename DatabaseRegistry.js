31ba5ea0ba6122f0d227ac57712eac14dc977c53
CardboardPL <mrplwaschilling@gmail.com>
2026-03-02 14:22:48 +0800

    chore(Database): add DatabaseRegistry skeleton

---
+++ DatabaseRegistry.js
@@ -0,0 +1,23 @@
+export class DatabaseRegistry {
+    #map;
+
+    constructor() {
+
+    }
+
+    registerConnection(id) {
+
+    }
+
+    deregisterConnection(id) {
+
+    }
+
+    async requestTransaction(name, id, transactionHandler) {
+
+    }
+
+    async requestUpgrade(name, id, upgradeHandler) {
+
+    }
+}
