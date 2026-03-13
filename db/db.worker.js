8584dbae1114e6d565d7ef37bec48bb80c96f77a
CardboardPL <mrplwaschilling@gmail.com>
2026-03-13 22:58:19 +0800

    chore(Database): remove db/db.worker.js

--- db/db.worker.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('message', (e) => {
-    const port = e.ports[0];
-    
-    if (port) {
-        if (e.data === 'Hub Worker Status Check') {
-            port.postMessage('Active');
-        }
-    }
-});
