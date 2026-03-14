65aaafdc56b6998c1b24d3759197121789057fda
CardboardPL <mrplwaschilling@gmail.com>
2026-03-14 21:47:01 +0800

    fix(db.worker): check for database worker instead of hub worker

--- workers/db.worker.js
+++ workers/db.worker.js
@@ -2,7 +2,7 @@ self.addEventListener('message', (e) => {
     const port = e.ports[0];
     
     if (port) {
-        if (e.data === 'Hub Worker Status Check') {
+        if (e.data === 'Database Worker Status Check') {
             port.postMessage('Active');
         }
     }
