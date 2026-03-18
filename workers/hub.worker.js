2b6ec20379223192ba28cb43c4e5ecb0ebf5be7f
CardboardPL <mrplwaschilling@gmail.com>
2026-03-18 21:05:32 +0800

    feat(hub.worker): add requests handling for the database worker

--- workers/hub.worker.js
+++ workers/hub.worker.js
@@ -1,3 +1,5 @@
+const requestsChannel = new BroadcastChannel('requests');
+const responsesChannel = new BroadcastChannel('responses');
 const dbChannel = new BroadcastChannel('db-channel');
 
 self.addEventListener('message', (e) => {
@@ -7,5 +9,54 @@ self.addEventListener('message', (e) => {
         if (e.data === 'Hub Worker Status Check') {
             port.postMessage('Active');
         }
+    }
+});
+
+requestsChannel.addEventListener('message', async (e) => {
+    const messageRequest = e.data;
+
+    if (!messageRequest) return;
+    
+    const { senderUUID, operationWorker } = messageRequest;
+
+    if (!senderUUID) {
+        console.error('Message sent without a senderUUID');
+        return;
+    }
+
+    try {
+        switch (operationWorker) {
+            case 'db':
+                const payload = {
+                    type: messageRequest.type
+                }
+                
+                await new Promise((resolve, reject) => {
+                    const timeoutId = setTimeout(() => {
+                        reject('Database failed to respond in time');
+                    }, 15000);
+                    dbChannel.onmessage = (e) => {
+                        clearTimeout(timeoutId);
+                        if (e.data === 'Success') {
+                            resolve();
+                            dbChannel.onmessage = null;
+                        } else {
+                            reject(e.data);
+                        }
+                    }
+
+                    dbChannel.postMessage(payload);
+                });
+                responsesChannel.postMessage({
+                    type: 'Status Check',
+                    status: 'Success',
+                    senderUUID
+                });
+                break;
+            default:
+                console.error('Invalid Operation Worker');
+        }
+    } catch(err) {
+        console.error(err);
     }
 });
