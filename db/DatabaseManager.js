5c6942d9dbe93e5f4c9bb74ba5ba3241825a0c55
CardboardPL <mrplwaschilling@gmail.com>
2026-04-19 22:34:02 +0800

    feat(db): add a DatabaseManager (skeleton) to manage multiple databases

---
+++ db/DatabaseManager.js
@@ -0,0 +1,21 @@
+import { Database } from './Database.js';
+
+export class DatabaseManager {
+    #databases;
+
+    constructor() {
+        this.#databases = new Map();
+    }
+
+    addDatabase(name) {
+
+    }
+
+    removeDatabase(name) {
+
+    }
+
+    getDatabase(name) {
+        
+    }
+}
