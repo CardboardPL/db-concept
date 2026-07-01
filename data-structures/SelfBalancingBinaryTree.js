6c9b95222d6bb62d6bdb92ffcead469bb32d9c48
CardboardPL <mrplwaschilling@gmail.com>
2026-07-01 23:15:45 +0800

    feat(data-structures): add initial SelfBalancingBinaryTree.js code
    - added a file named "SelfBalancingBinaryTree.js" to contain logic for a Self Balancing Binary Search Tree

---
+++ data-structures/SelfBalancingBinaryTree.js
@@ -0,0 +1,20 @@
+import { BinaryTreeNode } from "./BinaryTreeNode";
+
+class SelfBalancingBinaryTreeNode extends BinaryTreeNode {
+    constructor(data, weight, height, left, right) {
+        super(data, left, right);
+        this.weight = weight;
+        this.height = height;
+    }
+}
+
+class SelfBalancingBinaryTree {
+    #root;
+
+    constructor(data, weight) {
+        if (data == null) return;
+        this.#root = new SelfBalancingBinaryTreeNode(data, weight);
+
+        // TODO: Create balancing mechanism
+    }
+}
