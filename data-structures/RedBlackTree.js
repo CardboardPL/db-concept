571ef7d2ba9408632afc02ec6d4a2a159a1d0719
CardboardPL <mrplwaschilling@gmail.com>
2026-08-12 02:56:28 +0800

    fix(RedBlackTree): ensure that RedBlackTreeNode has a key property

--- data-structures/RedBlackTree.js
+++ data-structures/RedBlackTree.js
@@ -1,8 +1,10 @@
 import { BinaryTreeNode } from "./BinaryTreeNode.js";
 
 class RedBlackTreeNode extends BinaryTreeNode {
-    constructor(data, isRed = true) {
+    constructor(key, data, isRed = true) {
+        if (key == null || Number.isNaN(key)) throw new Error('Key must not be null, undefined, or NaN');
         super(data);
+        this.key = key;
         this.isRed = isRed;
     }
 }
