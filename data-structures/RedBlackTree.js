6cb80f077721351197020bb0550bc3da84d0fdd1
CardboardPL <mrplwaschilling@gmail.com>
2026-08-13 00:04:10 +0800

    feat(RedBlackTree): add stub for RedBlackTree class
    - added a stub for the RedBlackTree to outline the requirements for the upcoming RedBlackTree class

--- data-structures/RedBlackTree.js
+++ data-structures/RedBlackTree.js
@@ -8,3 +8,49 @@ class RedBlackTreeNode extends BinaryTreeNode {
         this.isRed = isRed;
     }
 }
+
+class RedBlackTree {
+    #root;
+
+    constructor(key, data) {
+        if (key == null) return;
+        this.#root = new RedBlackTreeNode(key, data, false);
+    }
+
+    /**
+     * Inserts a node into the tree
+     * @param {any} key - identifier of the node
+     * @param {any} data - data of the node
+     * @returns {Boolean} a boolean representing if a node was inserted or not
+     */
+    insert(key, data) {
+        
+    }
+
+    /** 
+     * Gets the data of a node given a key.
+     * @param {any} key - identifier of the target node
+     * @returns {any} the value of the target node or null if nothing was found
+     */
+    getData(key) {
+
+    }
+
+    /** 
+     * Updates the data of a node given a key.
+     * @param {any} key - identifier of the target node
+     * @returns {Boolean} a boolean representing if the node's data was updated or not
+     */
+    updateData(key) {
+
+    }
+
+    /** 
+     * Deletes a node from the tree given a key.
+     * @param {any} key - identifier of the target node
+     * @returns {Boolean} a boolean representing if the node was deleted or not
+     */
+    delete(key) {
+        
+    }
+}
